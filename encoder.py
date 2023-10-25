26d30317da38fae969677dd71e5e59007aa99e81
Noah Whelden <69485858+flarfo@users.noreply.github.com>
2023-10-25 09:39:32 -0400

    Updated encoder.py

--- encoder.py
+++ encoder.py
@@ -1,16 +1,26 @@
 class Encoder:
-    def __init__(self, password) -> None:
-        self.password = self.encode(password)
+    def __init__(self) -> None:
+        pass
 
     # Where password is an 8-digit string
     def encode(self, password: str):
         return ''.join([str(int(digit) + 3) for digit in password])
     
 def main():
-    password = input('Enter a password to encode: ')
-    encoder = Encoder(password)
+    encoder = Encoder()
 
-    print(encoder.password)
+    while True:
+        choice = input('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\nPlease enter an option: ')
+
+        match choice:
+            case '1':
+                encoder.encode(input('Please enter your password to encode: '))
+                print('Your password has been encoded and stored!\n')
+            case '2':
+                # Decode here
+                pass
+            case '3':
+                quit()
 
 if __name__ == '__main__':
     main()
